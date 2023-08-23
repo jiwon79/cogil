c131e023639d8a0541b5b4f1ab49c66da8bdda7f
pietis <ojs8879@snu.ac.kr>
2023-08-23 18:05:17 +0900

    디자인 적용

--- src/views/AboutButtonSelect.vue
+++ src/views/AboutButtonSelect.vue
@@ -1,9 +1,11 @@
 <template>
     <div>
-        <button v-for="(buttonText, index) in buttonList" :key="index" @click="changeText(index)">
-            {{ buttonText }}
-        </button>
-        <Document :index="selectedButton"/>
+        <div class = "button-area">
+            <button v-for="(buttonText, index) in buttonList" :key="index" @click="changeText(index)" class ="custom-button">
+                {{ buttonText }}
+            </button>
+        </div>
+        <Document :index="selectedButton"/> <!-- 세부 분야 -->
     </div>
 </template>
 
@@ -17,14 +19,14 @@ export default {
     },
     data() {
         return {
-            buttonList: ['버튼 1', '버튼 2', '버튼 3'], // 버튼 텍스트 목록
+            buttonList: ['게임 엔진', '수학/물리', '그래픽스', '인공지능'], // 버튼 텍스트 목록
             selectedButton: null, // 선택된 버튼의 인덱스
         };
     },  
     methods: {
         changeText(index) {   // 버튼 index 저장
             this.selectedButton = index;
-        },
+        }, 
         openLink1() {
             window.open("", "_blank");
         },
@@ -37,3 +39,47 @@ export default {
     },
 };
 </script>
+
+<style scoped>
+
+.button-area {
+    display: flex;
+    padding: 12px 16px;
+    align-items: flex-start;
+    gap: 4px;
+    align-self: stretch;
+background: var(--grey-100, #FFF);
+}
+
+.custom-button {
+    display: flex;
+    padding: 6px 12px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    background: #FFFFFF;
+    transition: background 0.1s ease;
+    border: none; /* 테두리 제거 */
+    color: var(--grey-600, #73767B);
+
+    /* Label/14 */
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: 16px; /* 114.286% */
+}
+
+.custom-button:active {
+    display: flex;
+    padding: 6px 12px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 100px;
+    background: var(--main-150, #FFDFE1);    
+    color: #E74A55;
+}
+
+</style>
+
