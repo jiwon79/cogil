0557c65acdd7205565aebc28535dd9892a82df47
mn39 <48751102+mn39@users.noreply.github.com>
2023-08-24 21:47:34 +0900

    home css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
-    <carouselCompo> </carouselCompo>
+    <div class="home-content-container">
+      <div class="home-content">
+        <h1>WEB</h1>
+        <div>내용 냉용 냉용ㅇㅇ 냉용ㅇ우어</div>
+      </div>
+    </div>
+    <div class="carousel-container">
+      <carouselCompo> </carouselCompo>
+    </div>
   </div>
 </template>
 
@@ -16,6 +24,50 @@ export default defineComponent({
 </script>
 
 <style scoped lang="scss">
+.home-content-container {
+  display: flex;
+  width: 255px;
+  height: 530px;
+  padding: 20px 24px;
+  margin: auto;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: flex-start;
+  flex: 1 0 0;
+
+  border-radius: 20px;
+  border: 8px solid var(--grey-100, #fff);
+  background: rgba(255, 234, 236, 0.6);
+  box-shadow: 2px 4px 12px 0px rgba(0, 0, 0, 0.1);
+}
+.home-content h1 {
+  color: var(--grey-800, #1d1e1f);
+
+  /* Heading/28 */
+  font-family: Pretendard;
+  font-size: 28px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 36px; /* 128.571% */
+  text-align: left;
+}
+.home-content div {
+  color: var(--grey-700, #3a3d41);
+
+  /* Paragraph/18 */
+  font-family: Pretendard;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 28px; /* 155.556% */
+}
+
+.carousel-container {
+  display: flex;
+  margin-top: 40px;
+  flex-direction: column;
+  align-items: center;
+}
 h3 {
   margin: 40px 0 0;
 }
