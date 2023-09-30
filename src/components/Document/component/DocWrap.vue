6f187881c892171b6b1bbef43fa34a617e3ad9b3
jiwon79 <19-079@ksa.hs.kr>
2023-10-01 00:39:36 +0900

    :sparkles: DocWrap 추가

---
+++ src/components/Document/component/DocWrap.vue
@@ -0,0 +1,15 @@
+<template>
+    <main class="main">
+        <slot/>
+    </main>
+</template>
+
+<style scoped lang="scss">
+
+.main {
+    width: 100%;
+    height: 100%;
+    background-color: #f5f5f5;
+}
+
+</style>
