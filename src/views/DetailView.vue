368f7f81cc827767ddd886c05c40271aae7be2b7
jiwon79 <19-079@ksa.hs.kr>
2023-10-02 18:33:05 +0900

    :sparkles: Detail 페이지 추가

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,12 +1,28 @@
 <template>
-    <MainHeader title="Test"/>
-    <p>test</p>
-    <p>{{ category}}</p>
+    <MainHeader :title="document.title"/>
+    <ButtonSelect
+        :label-list="labelList"
+        :cur-index="curIndex"
+        :set-cur-index="setCurIndex"
+    />
+    <Component :is="document.detailList[curIndex].content as any"/>
 </template>
 
 <script setup lang="ts">
-import MainHeader from "@/components/MainHeader.vue";
 import { useRoute } from "vue-router";
+import { ref } from "vue";
 
-const category = useRoute().params.category;
+import MainHeader from "@/components/MainHeader.vue";
+import ButtonSelect from "@/components/Document/component/ButtonSelect.vue";
+import documentMap from "@/components/Document/documentMap";
+
+const category = useRoute().params.category as string;
+const document = documentMap.get(category)!;
+const labelList = document.detailList.map((detail) => detail.title);
+
+const curIndex = ref(0)
+const setCurIndex = (index: number) => {
+  curIndex.value = index
+}
+
 </script>
