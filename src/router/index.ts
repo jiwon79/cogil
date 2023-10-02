63253fa497fac30af921eeb9f3ce9d3c55aa9423
jiwon79 <19-079@ksa.hs.kr>
2023-10-02 20:31:50 +0900

    :bug: router 정의되지 않은 페이지 제거

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import HomeView from "@/views/HomeView.vue";
+import DetailView from "@/views/DetailView.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -8,22 +9,11 @@ const routes: Array<RouteRecordRaw> = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-  {
     path: "/:category",
     name: "category",
     component: DetailView,
   }
 ];
-
-import DetailView from "../views/DetailView.vue";
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
